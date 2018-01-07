f2817ef80bb466476d50413113b8978087c7d989
lynn <907677243@qq.com>
2018-01-07 11:48:54 +0800

    修改内容： 1.完善了.gitignore文件 2.为Item、MainItem、SubItem三个类加了异常和注释

--- src/Job/SubItem.hpp
+++ src/Job/SubItem.hpp
@@ -1,13 +1,22 @@
 #ifndef SUBITEM_HPP
 #define SUBITEM_HPP
 
+#include "CustomException.hpp"
+#include "Alg2D.hpp"
 #include "Item.hpp"
 
 namespace Job
 {
     /**
      *  @brief SubItem
-     *
+     *          表示一个SubItem的类，存有以下信息：
+     *              1.所在lib的ID
+     *              2.当前SubItem的ID
+     *              3.与SubItem符合的矩形信息
+     *              4.SubItem的名称
+     *              5.SubItem使用的算法
+     *          具有以下功能：
+     *              模拟检测返回检测结果
      *  @author lynn
      *  @version 1.00 2018-01-05 lynn
      *                note:create it
@@ -19,8 +28,18 @@ namespace Job
         //>>>-----------------------------------------------------------------------
         // constructor & destructor
 
+        /**
+        *  @brief   默认构造函数，加入2D检测算法
+        *  @param   N/A
+        *  @return  N/A
+        */
         SubItem();
 
+        /**
+        *  @brief   析构函数,释放用于存放算法的空间
+        *  @param   N/A
+        *  @return  N/A
+        */
         virtual ~SubItem();
 
         //>>>-----------------------------------------------------------------------
@@ -35,7 +54,7 @@ namespace Job
         //>>>-----------------------------------------------------------------------
         // member variant
 
-        int m_order{0};
+        int m_order{0};     //记录检测顺序
 
         //<<<-----------------------------------------------------------------------
     };
