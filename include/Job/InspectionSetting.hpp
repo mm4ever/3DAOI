1f6838d75104a5f0e530c9a803edae9267996327
lynn <907677243@qq.com>
2018-01-13 21:45:34 +0800

    修改内容： 1.完成了InspectionSetting和InspectionData类（包括异常和注释） 2.修改了Board类的inspect函数，改为先检测FiducialMark后检测其它元件

--- include/Job/InspectionSetting.hpp
+++ include/Job/InspectionSetting.hpp
@@ -10,7 +10,7 @@
 
 namespace Job
 {
-    //图像的位数,目前支持8位和16位
+    //图像的位数,表示将从黑到白的颜色映射到2的ImgBit次方个数字
     enum class ImgBit
     {
         BIT8,
@@ -18,9 +18,12 @@ namespace Job
     };
 
     /**
-     *  @brief  CaptureSetting
-     *          相机参数配置文件的设置:
-     *          具体参数如:相机宽方向,高方向的像素数,相机图像的位数等
+     *  @brief  InspectionSetting
+     *          存有以下信息：
+     *          相机长宽(单位：像素),相机图像的位数等
+     *          具有以下功能:
+     *          读取inspectionSetting配置文件并保存信息
+     *
      *  @author lynn
      *  @version 1.00 2018-01-05 lynn
      *                note:create it
@@ -31,8 +34,18 @@ namespace Job
         //>>>-----------------------------------------------------------------------
         // constructor & deconstructor
 
+        /**
+        *  @brief   默认构造函数
+        *  @param   N/A
+        *  @return  N/A
+        */
         InspectionSetting();
 
+        /**
+        *  @brief   析构函数
+        *  @param   N/A
+        *  @return  N/A
+        */
         virtual ~InspectionSetting();
 
         //>>>-----------------------------------------------------------------------
@@ -42,29 +55,19 @@ namespace Job
 
         int imgHeight() {return this->m_imgHeight;}
 
+        ImgBit imgBit() {return this->m_imgBit;}
+
         //>>>-----------------------------------------------------------------------
         // member function
 
         /**
-        *  @brief  readCaptureSetting
-        *          读取指定目录下的文件,判断文件目录是否存在,如果不存在则抛出异常
-        *          如果文件数据不正确,则抛出异常
+        *  @brief  load
+        *          加载配置文件,如果文件不存在或文件数据不正确则抛出异常
+        *
         *  @param  path:读取文件的路径,如(../config/CaptureSetting.ini)
         *  @return  N/A
         */
-        void readCaptureSetting(const QString& path);
-
-        /**
-        *  @brief  writeCaptureSetting
-        *          将根据数据格式将数据写入指定目录下的文件
-        *  @param  path:写入数据文件的路径,如(../config/CaptureSetting.ini)
-        *  @return  N/A
-        */
-        void writeCaptureSetting(const QString& path);
-
-        void init();
-
-        void load(std::string& path);
+        void load(QString& path);
 
 
     private:
